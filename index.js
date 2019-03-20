c85be78c10617b58aafc389b1bdea94243917d96
Nikita Machekhin <nicitamac@gmail.com>
2019-03-20 16:40:10 +0200

    Update dependencies

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
-// 'use strict';
+'use strict';
 
 const http = require('http');
 
-
 const proxy = http.createServer((req, res) => {
-  console.log(req.url);
-
   const options = {
     host: req.headers.host,
     headers: req.headers,
     method: req.method
-  }
-  console.log(options);
-  // console.log(options);
-
+  };
+  // res.end('end');
   const requ = http.request(options, res1 => {
-    console.log(1);
-    res1.on('data', chunk => console.log(chunk));
+    // res1.on('data', chunk => console.log(chunk));
     res1.pipe(res);
   });
+  // setTimeout(() => {res.end('srtr'), 3000});
   requ.end();
 });
 
-proxy.listen(2001, () => console.log('Listen on 2001'));
+proxy.listen(2001);
